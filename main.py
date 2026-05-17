1287bd6882b49c66e0c2597113e73e50a95930cd
AJ-18 <arjun.suresh1@marist.edu>
2026-05-16 21:57:41 -0400

    Credit card validation with inheritance

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ df = pandas.read_csv("hotels.csv", dtype={"id":str})
 # Loading the dataframe as a dictionary
 df_cards = pandas.read_csv("fake_cards.csv", dtype=str).to_dict(orient="records")
 
+df_cards_security = pandas.read_csv("card_security.csv", dtype=str)
+
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
@@ -38,6 +40,7 @@ class ReservationTicket:
         """
         return content
 
+
 class CreditCard:
     def __init__(self, number):
         self.number = number
@@ -50,6 +53,16 @@ class CreditCard:
         else:
             return False
 
+
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
@@ -57,12 +70,15 @@ hotel = Hotel(hotel_id)
 if hotel.available():
     # What is the minimum number of parameters needed when defining an instance?
     # - Design the class around that, CreditCard in this case.
-    credit_card = CreditCard(number="1234567897676767")
+    credit_card = SecureCreditCard(number="1234567897676767")
     if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(customer_name = name, hotel_object = hotel)
-        print(reservation_ticket.generate())
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name = name, hotel_object = hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Credit card authentication failed.")
     else:
         print("There was a problem with your payment.")
 else:
